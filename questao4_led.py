7a1364c245ad4b51b7091aecad9337d751284e6f
LexMoraes <154477172+LexMoraes@users.noreply.github.com>
2024-03-20 15:13:28 -0400

    Add files via upload
    4 – LED
    
    João quer montar um painel de leds contendo diversos números. Ele não possui muitos leds, e não tem certeza se conseguirá montar o número desejado. Considerando a configuração dos leds dos números abaixo, faça um algoritmo que ajude João a descobrir a quantidade de leds necessário para montar o valor.
    
    Obs.: Para programadores de Javascript, recomenda-se o uso de "input.trim().split('\n')" para evitar erros conhecidos.
    
    Ver figura abaixo.
    
    Entrada
    
    A entrada contém um inteiro N, (1 ≤ N ≤ 1000) correspondente ao número de casos de teste, seguido de N linhas, cada linha contendo um número (1 ≤ V ≤ 10¹⁰⁰) correspondente ao valor que João quer montar com os leds.
    
    Saída
    
    Para cada caso de teste, imprima uma linha contendo o número de leds que João precisa para montar o valor desejado, seguido da palavra "leds".

---
+++ questao4_led.py
@@ -0,0 +1,28 @@
+def qtde_leds(num):
+    # Lista com a quantidade de leds para cada número digitado
+    led_count = {
+        '1': 2,  # Um led para o segmento superior e outro para o inferior
+        '2': 5,  # Cinco leds: dois superiores, dois inferiores e um central
+        '3': 5,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 3,
+        '8': 7,
+        '9': 6,
+        '0': 6
+    }
+
+    total_leds = 0
+    for digito in str(num):
+        total_leds += led_count[digito]
+
+    return total_leds
+
+# Leitura do número de casos de teste
+N = int(input("Digite o valor que João quer exibir nos leds"))
+
+for _ in range(1):
+    value = 0
+    leds_necessarias = qtde_leds(value)
+    print(f"{leds_necessarias} leds")
