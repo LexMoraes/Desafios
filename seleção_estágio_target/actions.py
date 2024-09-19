251bb9c7a9d3b29d6797004dc9076f697351d3e6
LexMoraes <154477172+LexMoraes@users.noreply.github.com>
2024-09-19 15:41:15 +0000

    Organização do código e comentários

--- seleção_estágio_target/actions.py
+++ seleção_estágio_target/actions.py
@@ -1,11 +1,10 @@
 from view import *
 
-
 # Módulo para desenvolver a lógica e chamadas entre todas as questões do teste.
 
 
-# Classe para trabalhar a lógica da questão 1 - sequência de Fibonacci.
-class FibonacciActions:
+# Classe para trabalhar a lógica da questão 1 - Sequência de Fibonacci.
+class QuestionOne:
     def __init__(self, num):
         self.fib_sequence = [0, 1]
         self.num = num
@@ -33,9 +32,12 @@ class FibonacciActions:
         if self.num in self.fib_sequence:
             print(f"O número {self.num} existe na sequência de Fibonacci.")
             print("Sequência até o número informado:", self.fib_sequence)
+            print('')
         else:
             print(f"O número {self.num} não existe na sequência de Fibonacci.")
+            print('')
 
+# Classe para trabalhar a lógica da questão 2 - Encontrar vogal 'a'.
 class QuestionTwo:
     def __init__(self, input_string):
         self.input_string = input_string
@@ -48,15 +50,16 @@ class QuestionTwo:
         # Conta o número de ocorrências da vogal 'a'
         count = input_string.count('a')
         print(f'Existem {count} vogais "a" na string')
+        print('')
         return
 
-
+# Classe para trabalhar a lógica da questão 3 - Repetição de soma.
 class QuestionThree:
     def __init__(self):
         pass
 
     def result_three(self):
-        print('''3) Observe o trecho de código abaixo: int INDICE = 12,\n SOMA = 0, K = 1; enquanto K < INDICE \nfaça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
+        print('''\n3) Observe o trecho de código abaixo:\nint INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE\nFaça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
         ''')
         indice = 12
         soma = 0
@@ -64,30 +67,48 @@ class QuestionThree:
         while k < indice:
             k = k + 1
             soma = soma + k
-        print(f'O resultado da soma [e: {soma}')
+        print(f'Resposta: o resultado da soma é: {soma}')
+        print('')
 
-
+# Classe para armazenar as respostas da questão 4 - Lógica de sequências.
 class QuestionFour:
     def __init__(self):
         pass
     def result_four(self):
-        print('4) Resposta(R):')
-        print('')
+        print('\n4) Resposta(R):\n')
+
         print('Progressão aritmética de 2.')
-        print('a) 1, 3, 5, 7, ___. R: 9')
-        print('')
+        print('a) 1, 3, 5, 7, ___. Result: 9\n')
+
         print('Progressão geométrica de 2.')
-        print('b) 2, 4, 8, 16, 32, 64, ___. R: 128')
-        print('')
+        print('b) 2, 4, 8, 16, 32, 64, ___. Result: 128\n')
+
         print('Quadrados perfeitos')
-        print('c) 0, 1, 4, 9, 16, 25, 36, ___. R:')
-        print('')
-        print('Quadrados perfeitos, apenas pares, iniciando com 2²')
-        print('d) 4, 16, 36, 64, ___. R:')
-        print('')
+        print('c) 0, 1, 4, 9, 16, 25, 36, ___. Result: 49\n')
+
+        print('Quadrados perfeitos, apenas base pares, iniciando com 2²')
+        print('d) 4, 16, 36, 64, ___. Result: 100\n')
+
         print('Encadeamento do próximo valor = atual + anterior (Fibonacci)')
-        print('e) 1, 1, 2, 3, 5, 8, ___. R: 13')
-        print('')
+        print('e) 1, 1, 2, 3, 5, 8, ___. Result: 13\n')
+
         print('Após o 16, é necessário abstrair os valores anteriores, considerando progressão é de 1, em 1')
-        print('f) 2,10, 12, 16, 17, 18, 19, ___. R: 20')
+        print('f) 2,10, 12, 16, 17, 18, 19, ___. Result: 20\n')
+
+# Classe para armazenar as respostas da questão 5 - Enígma da lâmpada.
+class QuestionFive:
+    def __init__(self):
+        pass
+
+    def result_five(self):
+        print('5) Resposta:')
+        print('Passo 1 - Ativar um dos interruptores')
+        print('Passo 2 - Aguardar alguns minutos.')
+        print('Passo 3 - Desligar o interruptor ativado')
+        print('Passo 4 - Ativar outro interruptor.')
+        print('Passo 5 - Checar as salas para verificar as lâmpadas: quente, fria e ligada')
+        print('A lâmpada mais quente; primeiro interruptor ativado.')
+        print('A lâmpada mais fria; interruptor que não foi ativado.')
+        print('A lâmpada ligada; último interruptor ativado.')
+        print('')
 
--- seleção_estágio_target/view.py
+++ seleção_estágio_target/view.py
@@ -1,6 +1,6 @@
 from dbm import error
 
-from actions import FibonacciActions, QuestionThree, QuestionFour, QuestionTwo
+from actions import QuestionOne, QuestionThree, QuestionFour, QuestionTwo, QuestionFive
 
 
 class MenuView:
@@ -10,9 +10,9 @@ class MenuView:
     def show_menu(self):
         print("Qual questão do desafio você deseja testar?")
         print("1. Sequência de Fibonacci.")
-        print("2. Contagem da vogal 'a'.")
-        print("3. Laço de repetição.")
-        print("4. Descubra a lógica e complete.")
+        print("2. Encontrar vogal 'a'.")
+        print("3. Repetição de soma.")
+        print("4. Lógica de sequências.")
         print("5. Enígma da lâmpada.")
         print("0. Sair do programa.")
 
@@ -26,10 +26,9 @@ class MenuView:
                     print('Esta parte do programa deduz se o número informado faz parte da sequência de Fibonacci!')
                     print('Insira um número para verificar a existência na sequência de Fibonacci!')
                     num = input('Insira aqui: ')
-                    question_one = FibonacciActions(num=num)
+                    question_one = QuestionOne(num=num)
                     question_one.validade_num()
                     question_one.check_fibonacci()
-                    print('')
                     self.show_menu()
                     self.menu_option()
                 case '2':
@@ -39,32 +38,21 @@ class MenuView:
                     enter = input('Insira aqui uma string para verificar: ')
                     question_two = QuestionTwo(enter)
                     question_two.count_a_vowels()
-                    print('')
                     self.show_menu()
                     self.menu_option()
                 case '3':
                     question = QuestionThree()
                     question.result_three()
-                    print('')
                     self.show_menu()
                     self.menu_option()
                 case '4':
                     question = QuestionFour()
                     question.result_four()
-                    print('')
                     self.show_menu()
                     self.menu_option()
                 case '5':
-                    print('5) Resposta:')
-                    print('Passo 1 - Ativar um dos interruptores')
-                    print('Passo 2 - Aguardar alguns minutos.')
-                    print('Passo 3 - Desligar o interruptor ativado')
-                    print('Passo 4 - Ativar outro interruptor.')
-                    print('Passo 5 - Checar as salas para verificar as lâmpadas: quente, fria e ligada')
-                    print('A lâmpada mais quente; primeiro interruptor ativado.')
-                    print('A lâmpada mais fria; interruptor que não foi ativado.')
-                    print('A lâmpada ligada; último interruptor ativado.')
-                    print('')
+                    question = QuestionFive()
+                    question.result_five()
                     self.show_menu()
                     self.menu_option()
                 case '0':
